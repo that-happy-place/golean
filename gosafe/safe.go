e532dd350366626d92ceae74085b8661b8da91e8
Udayabharathi <94507000+udayabharathi-t@users.noreply.github.com>
2025-03-12 18:18:24 +0530

    Add line number of panic stack trace

--- gosafe/safe.go
+++ gosafe/safe.go
@@ -2,13 +2,16 @@ package gosafe
 
 import (
 	"fmt"
+	"runtime"
 )
 
 func safe(f func() error) func() error {
 	return func() (err error) {
 		defer func() {
 			if r := recover(); r != nil {
-				err = fmt.Errorf("panic: occurred in goroutine: %+v", r)
+				buf := make([]byte, 1024)
+				n := runtime.Stack(buf, false) // Capture the stack trace
+				err = fmt.Errorf("panic: occurred in goroutine: %+v, Stack trace: %s", r, string(buf[:n]))
 			}
 		}()
 		return f()
