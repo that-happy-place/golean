3368b74ff0074f25781ee4c752b220190ed8bf03
Udayabharathi <94507000+udayabharathi-t@users.noreply.github.com>
2026-08-19 11:41:33 +0530

    Add FilterSlice function for filtering slices

--- collections/streams.go
+++ collections/streams.go
@@ -12,3 +12,18 @@ func TransformSlice[F any, T any](transformer func(src F) T, sourceSlice []F) []
 
 	return to
 }
+
+func FilterSlice[T any](filter func(src T) bool, sourceSlice []T) []T {
+	if len(sourceSlice) == 0 {
+		return nil
+	}
+
+	var to []T
+	for _, ele := range sourceSlice {
+		if filter(ele) {
+			to = append(to, ele)
+		}
+	}
+
+	return to
+}
